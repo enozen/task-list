18984647062dc9b0d9ddd2c8b34e14c4ff93ceb2
Hendri Zeneli <hendrizeneli@yahoo.com>
2023-09-27 11:20:34 +0200

    first step

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import cors from "cors";
+import taskRouter from "./routes/taskRouter.js";
+
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.use(cors());
+app.use(express.json()); // body parser erlaubt uns auf Daten aus dem req.body zuzugreifen
+
+app.use("/tasks", taskRouter);
+
+app.use((req, res, next)=>{
+    res.status(404).send("Page not found!");
+});
+
+app.use((err, req, res, next)=>{
+    console.log("General error handler", err);
+    res.status(500).json(err);
+})
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server is listening on port: ${PORT}`);
+});
